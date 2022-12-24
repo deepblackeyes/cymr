807cb84a98c72cb64b550daa90579a62cdbb920f
whwwh <jmr115542@gmail.com>
2022-12-24 18:10:49 +0900

    test 코드 수정

--- src/test/java/com/jojoldu/book/springboot/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jojoldu/book/springboot/springboot/web/HelloControllerTest.java
@@ -1,18 +1,19 @@
 package com.jojoldu.book.springboot.springboot.web;
 
 import com.jojoldu.book.springboot.web.HelloController;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
     @Autowired
@@ -28,4 +29,7 @@ public class HelloControllerTest {
 
     }
 
+
 }
+
+
